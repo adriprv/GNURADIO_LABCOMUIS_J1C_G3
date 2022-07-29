0b2937bdb551816fd6c014706019b91a2d0b7931
adriprv <104648972+adriprv@users.noreply.github.com>
2022-07-29 16:10:06 -0500

    Add files via upload

---
+++ lab3_2/CalculoPotenciaComuG03.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: CalculoPotenciaComuG03
+# Author: J1C
+# GNU Radio version: 3.9.5.0
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+import sip
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import fft
+from gnuradio.fft import window
+from gnuradio import gr
+from gnuradio.filter import firdes
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import Modulos_J1C03
+
+
+
+from gnuradio import qtgui
+
+class CalculoPotenciaComuG03(gr.top_block, Qt.QWidget):
+
+    def __init__(self, I_vect=1024):
+        gr.top_block.__init__(self, "CalculoPotenciaComuG03", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("CalculoPotenciaComuG03")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "CalculoPotenciaComuG03")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.I_vect = I_vect
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.qtgui_number_sink_2 = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1,
+            None # parent
+        )
+        self.qtgui_number_sink_2.set_update_time(0.10)
+        self.qtgui_number_sink_2.set_title("Potencia lineal [W]")
+
+        labels = ['Potencia', '', '', '', '',
+            '', '', '', '', '']
+        units = ['[W]', '', '', '', '',
+            '', '', '', '', '']
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+
+        for i in range(1):
+            self.qtgui_number_sink_2.set_min(i, -1)
+            self.qtgui_number_sink_2.set_max(i, 1)
+            self.qtgui_number_sink_2.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_2.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_2.set_label(i, labels[i])
+            self.qtgui_number_sink_2.set_unit(i, units[i])
+            self.qtgui_number_sink_2.set_factor(i, factor[i])
+
+        self.qtgui_number_sink_2.enable_autoscale(False)
+        self._qtgui_number_sink_2_win = sip.wrapinstance(self.qtgui_number_sink_2.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_number_sink_2_win)
+        self.qtgui_number_sink_1 = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1,
+            None # parent
+        )
+        self.qtgui_number_sink_1.set_update_time(0.10)
+        self.qtgui_number_sink_1.set_title("potencia Log [dBW]")
+
+        labels = ['Potencia', '', '', '', '',
+            '', '', '', '', '']
+        units = ['dBw', '', '', '', '',
+            '', '', '', '', '']
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+
+        for i in range(1):
+            self.qtgui_number_sink_1.set_min(i, -1)
+            self.qtgui_number_sink_1.set_max(i, 1)
+            self.qtgui_number_sink_1.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_1.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_1.set_label(i, labels[i])
+            self.qtgui_number_sink_1.set_unit(i, units[i])
+            self.qtgui_number_sink_1.set_factor(i, factor[i])
+
+        self.qtgui_number_sink_1.enable_autoscale(True)
+        self._qtgui_number_sink_1_win = sip.wrapinstance(self.qtgui_number_sink_1.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_number_sink_1_win)
+        self.qtgui_number_sink_0 = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1,
+            None # parent
+        )
+        self.qtgui_number_sink_0.set_update_time(0.10)
+        self.qtgui_number_sink_0.set_title("Potencia Log [dBm]")
+
+        labels = ['Potencia', '', '', '', '',
+            '', '', '', '', '']
+        units = ['[dBm]', '', '', '', '',
+            '', '', '', '', '']
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+
+        for i in range(1):
+            self.qtgui_number_sink_0.set_min(i, -1)
+            self.qtgui_number_sink_0.set_max(i, 1)
+            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_0.set_label(i, labels[i])
+            self.qtgui_number_sink_0.set_unit(i, units[i])
+            self.qtgui_number_sink_0.set_factor(i, factor[i])
+
+        self.qtgui_number_sink_0.enable_autoscale(True)
+        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_number_sink_0_win)
+        self.fft_vxx_0 = fft.fft_vfc(I_vect, True, window.blackmanharris(1024), True, 1)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, I_vect)
+        self.blocks_nlog10_ff_1 = blocks.nlog10_ff(10, 1, 30)
+        self.blocks_nlog10_ff_0 = blocks.nlog10_ff(10, 1, 0)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(1/(2*135115.625))
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(I_vect)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 1000, 4, 0, 0)
+        self.Modulos_J1C03_sumavect_0 = Modulos_J1C03.sumavect(I_vect)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.Modulos_J1C03_sumavect_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.Modulos_J1C03_sumavect_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_nlog10_ff_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_nlog10_ff_1, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_number_sink_2, 0))
+        self.connect((self.blocks_nlog10_ff_0, 0), (self.qtgui_number_sink_1, 0))
+        self.connect((self.blocks_nlog10_ff_1, 0), (self.qtgui_number_sink_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
+        self.connect((self.fft_vxx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "CalculoPotenciaComuG03")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_I_vect(self):
+        return self.I_vect
+
+    def set_I_vect(self, I_vect):
+        self.I_vect = I_vect
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+
+
+def argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--I-vect", dest="I_vect", type=intx, default=1024,
+        help="Set Longitud FFT [default=%(default)r]")
+    return parser
+
+
+def main(top_block_cls=CalculoPotenciaComuG03, options=None):
+    if options is None:
+        options = argument_parser().parse_args()
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(I_vect=options.I_vect)
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
